dc4f8593c16490d99e2ef76c9e1943f365b6d91d
KayOI <kay@onlineimage.com>
2026-09-24 08:40:03 -0600

    added nonce value to script-src

--- internal/httpserver/middleware.go
+++ internal/httpserver/middleware.go
@@ -69,7 +69,8 @@ func noSniff(next http.Handler) http.Handler {
 
 func contentSecurityPolicy(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(responseWriter http.ResponseWriter, request *http.Request) {
-		responseWriter.Header().Set("Content-Security-Policy", "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:; frame-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self'")
+		nonce := httpx.CSPNonce(request.Context())
+		responseWriter.Header().Set("Content-Security-Policy", "default-src 'self'; script-src 'self' 'nonce-"+nonce+"'; style-src 'self'; img-src 'self' data:; frame-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self'")
 		next.ServeHTTP(responseWriter, request)
 	})
 }
