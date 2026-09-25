6909d632a822426dc9a3b18578b3e605ab021caa
KayOI <kay@onlineimage.com>
2026-09-25 07:51:25 -0600

    checking for env variable

--- internal/config/config.go
+++ internal/config/config.go
@@ -42,6 +42,14 @@ type AttackerLabConfig struct {
 	Port int
 }
 
+func requireEnvVariable(environment map[string]string, name string) (string, error) {
+	value := environment[name]
+	if value == "" {
+		return "", fmt.Errorf("missing required environment variable: %s", name)
+	}
+	return value, nil
+}
+
 func Load(workingDirectory string) (Config, error) {
 	return Parse(processEnvironment(), workingDirectory)
 }
@@ -51,6 +59,11 @@ func LoadAttackerLab(workingDirectory string) (AttackerLabConfig, error) {
 }
 
 func Parse(environment map[string]string, workingDirectory string) (Config, error) {
+	apikey, err := requireEnvVariable(environment, "PAWPAL_API_KEY")
+	if err != nil {
+		return Config{}, err
+	}
+
 	port, err := parseNonNegativeInteger(valueOrDefault(environment, "PORT", strconv.Itoa(defaultPort)), "PORT")
 	if err != nil {
 		return Config{}, err
@@ -79,7 +92,7 @@ func Parse(environment map[string]string, workingDirectory string) (Config, erro
 	}
 
 	return Config{
-		PawPalAPIKey:               "bs_test_pawpal_starter_key",
+		PawPalAPIKey:               apikey,
 		AppOrigin:                  appOrigin,
 		Port:                       port,
 		DatabasePath:               databasePath,
